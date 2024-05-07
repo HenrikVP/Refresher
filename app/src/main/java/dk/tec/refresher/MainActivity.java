137bfa8bffe2931f0dc8e441184c62f273511940
HenrikVP <hpo@tec.dk>
2024-05-07 12:57:06 +0200

    Init

---
+++ app/src/main/java/dk/tec/refresher/MainActivity.java
@@ -0,0 +1,116 @@
+package dk.tec.refresher;
+
+import static android.Manifest.*;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import dk.tec.refresher.Model.Food;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static List<Food> foodlist;
+
+    FusedLocationProviderClient flpc;
+    TextView txt_lon, txt_lat, txt_alt, txt_dir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        Log.d("MainActivity", "onCreate: starting app");
+
+        permissionsList = new ArrayList<>();
+        permissionsList.addAll(Arrays.asList(permissionsStr));
+        askForPermissions(permissionsList);
+
+        flpc = LocationServices.getFusedLocationProviderClient(getApplicationContext());
+
+        foodlist = new ArrayList<>();
+        EditText input = findViewById(R.id.et_food);
+        Button mrButton = findViewById(R.id.btn_savefood);
+
+        mrButton.setOnClickListener(view ->
+                foodlist.add(new Food(input.getText().toString()))
+        );
+    }
+
+
+    ArrayList<String> permissionsList;
+    String[] permissionsStr = {
+            permission.CAMERA,
+            permission.RECORD_AUDIO,
+            permission.READ_EXTERNAL_STORAGE,
+            permission.ACCESS_FINE_LOCATION};
+    int permissionsCount = 0;
+    ActivityResultLauncher<String[]> permissionsLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+                    new ActivityResultCallback<Map<String, Boolean>>() {
+                        @RequiresApi(api = Build.VERSION_CODES.M)
+                        @Override
+                        public void onActivityResult(Map<String, Boolean> result) {
+
+                        }
+                    });
+
+    private void askForPermissions(ArrayList<String> permissionsList) {
+        String[] newPermissionStr = new String[permissionsList.size()];
+        for (int i = 0; i < newPermissionStr.length; i++) {
+            newPermissionStr[i] = permissionsList.get(i);
+        }
+        if (newPermissionStr.length > 0) {
+            permissionsLauncher.launch(newPermissionStr);
+        } else {
+        /* User has pressed 'Deny & Don't ask again' so we have to show the enable permissions dialog
+        which will lead them to app details page to enable permissions from there. */
+            showPermissionDialog();
+        }
+    }
+
+    AlertDialog alertDialog;
+
+    private void showPermissionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permission required")
+                .setMessage("Some permissions are need to be allowed to use this app without any problems.")
+                .setPositiveButton("Settings", (dialog, which) -> {
+                    dialog.dismiss();
+                });
+        if (alertDialog == null) {
+            alertDialog = builder.create();
+            if (!alertDialog.isShowing()) {
+                alertDialog.show();
+            }
+        }
+    }
+}
