f0b8d4c765bfce135d702b667789d819bc5dccc7
HenrikVP <hpo@tec.dk>
2024-05-08 11:00:37 +0200

    Dazed & Confused

--- app/src/main/java/dk/tec/refresher/MainActivity.java
+++ app/src/main/java/dk/tec/refresher/MainActivity.java
@@ -5,26 +5,18 @@ import static android.Manifest.*;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -38,97 +30,97 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import dk.tec.refresher.Model.Food;
+import dk.tec.refresher.Model.ToiletLocation;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static List<Food> foodlist;
-
+    public static List<ToiletLocation> toilets;
+    Location location;
     FusedLocationProviderClient flpc;
     TextView txt_lon, txt_lat, txt_alt, txt_dir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        Log.d("MainActivity", "onCreate: starting app");
 
+        // Initialize GUI elements
         initGui();
-        permissionsList = new ArrayList<>();
+
+        // Initialize permissions list
+        ArrayList<String> permissionsList = new ArrayList<>();
         permissionsList.addAll(Arrays.asList(permissionsStr));
+
+        // Request necessary permissions
         askForPermissions(permissionsList);
 
+        // Initialize FusedLocationProviderClient
         flpc = LocationServices.getFusedLocationProviderClient(getApplicationContext());
 
-        foodlist = new ArrayList<>();
-        EditText input = findViewById(R.id.et_food);
-        Button mrButton = findViewById(R.id.btn_savefood);
+        // Initialize food list and set button click listener
+        toilets = new ArrayList<>();
 
-        mrButton.setOnClickListener(view ->
-                foodlist.add(new Food(input.getText().toString()))
-        );
+        //mrButton.setOnClickListener(view -> foodlist.add(new Food(input.getText().toString())));
     }
 
+
+    // Initialize GUI elements
     private void initGui() {
         txt_lat = findViewById(R.id.txt_lat);
         txt_lon = findViewById(R.id.txt_lon);
         txt_alt = findViewById(R.id.txt_alt);
         txt_dir = findViewById(R.id.txt_dir);
+
+        findViewById(R.id.btn_savetoiletlocation).setOnClickListener(view -> {
+            new ToiletLocation(location);
+
+        });
     }
 
+    // Request location updates
     private void getUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission not granted, handle it here or request permission again
+            return;
+        }
+
+        flpc = LocationServices.getFusedLocationProviderClient(this);
         LocationRequest locationRequest = new LocationRequest.Builder(
                 Priority.PRIORITY_HIGH_ACCURACY, 1000).build();
 
-        flpc = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
         flpc.requestLocationUpdates(locationRequest, new LocationCallback() {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
-                Location location = locationResult.getLastLocation();
+                location = locationResult.getLastLocation();
+
+                // Update UI with location information
                 txt_lat.setText("Latitude: " + location.getLatitude());
                 txt_lon.setText("Longitude: " + location.getLongitude());
                 txt_alt.setText("Altitude: " + location.getAltitude());
                 txt_dir.setText("Direction: " + location.getBearing());
             }
         }, Looper.myLooper());
-
     }
 
-    //region Permission
+    // Permissions handling
     ArrayList<String> permissionsList;
     String[] permissionsStr = {
             permission.ACCESS_FINE_LOCATION,
             permission.ACCESS_COARSE_LOCATION,
             permission.CAMERA,
-            permission.ACCESS_BACKGROUND_LOCATION,
             permission.RECORD_AUDIO
     };
-    int permissionsCount = 0;
+
+    // Register permission request launcher
     ActivityResultLauncher<String[]> permissionsLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
                     new ActivityResultCallback<Map<String, Boolean>>() {
-                        @RequiresApi(api = Build.VERSION_CODES.M)
                         @Override
                         public void onActivityResult(Map<String, Boolean> result) {
                             ArrayList<Boolean> list = new ArrayList<>(result.values());
                             permissionsList = new ArrayList<>();
-                            permissionsCount = 0;
+                            int permissionsCount = 0;
                             for (int i = 0; i < list.size(); i++) {
                                 if (shouldShowRequestPermissionRationale(permissionsStr[i])) {
                                     permissionsList.add(permissionsStr[i]);
@@ -136,43 +128,43 @@ public class MainActivity extends AppCompatActivity {
                                     permissionsCount++;
                                 }
                             }
-                            if (permissionsList.size() > 0) {
-                                //Some permissions are denied and can be asked again.
+                            if (!permissionsList.isEmpty()) {
+                                // Request permissions again for ones that were denied
                                 askForPermissions(permissionsList);
                             } else if (permissionsCount > 0) {
-                                //Show alert dialog
+                                // Show permission dialog for permissions that cannot be requested again
                                 showPermissionDialog();
                             } else {
-                                //All permissions granted. Do your stuff 🤞
+                                // All permissions granted, proceed with necessary actions
                                 getUpdates();
                             }
                         }
                     });
 
+    // Check if permission is granted
     private boolean hasPermission(Context context, String permissionStr) {
         return ContextCompat.checkSelfPermission(context, permissionStr) == PackageManager.PERMISSION_GRANTED;
     }
 
+    // Request permissions
     private void askForPermissions(ArrayList<String> permissionsList) {
         String[] newPermissionStr = new String[permissionsList.size()];
-        for (int i = 0; i < newPermissionStr.length; i++) {
-            newPermissionStr[i] = permissionsList.get(i);
-        }
+        newPermissionStr = permissionsList.toArray(newPermissionStr);
         if (newPermissionStr.length > 0) {
+            // Launch permission request
             permissionsLauncher.launch(newPermissionStr);
         } else {
-        /* User has pressed 'Deny & Don't ask again' so we have to show the enable permissions dialog
-        which will lead them to app details page to enable permissions from there. */
+            // Show dialog to guide user to app settings for enabling permissions
             showPermissionDialog();
         }
     }
 
+    // Show permission dialog
     AlertDialog alertDialog;
-
     private void showPermissionDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Permission required")
-                .setMessage("Some permissions are need to be allowed to use this app without any problems.")
+                .setMessage("Some permissions are needed to be allowed to use this app without any problems.")
                 .setPositiveButton("Continue", (dialog, which) -> {
                     dialog.dismiss();
                 });
@@ -183,5 +175,4 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    //endregion
 }
